6df37215a904afb5cd9abdcfbc0cb2b37603fd04
Bayero Abass Barry <bayeroabassb@gmail.com>
2021-07-27 18:31:27 +0000

    Move static pages to pages folder and remove link to about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -16,7 +16,7 @@
         <p><a href="/">Revenir sur la page d'acceuil</a></p>
 
         <footer>
-            <p>&copy; Copyright  {{date('Y')}} &middot; <a href="/about-us">About Us</a></p>
+            <p>&copy; Copyright  {{date('Y')}}</p>
         </footer>
     </body>
 </html>
