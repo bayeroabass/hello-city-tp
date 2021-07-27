b5c5c5f6ec6ddbaa23092fabbcb121c54c690c44
Bayero Abass Barry <bayeroabassb@gmail.com>
2021-07-27 18:20:42 +0000

    Add About page and link

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,17 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello city</title>
+        <title>About us | Hello city</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
 
     <body>
-        <h1>Hello from Conakry !</h1>
 
-        <p>It's currently {{date('h:i A')}}.</p>
-
+        <p>Built with &hearts; by LES TEACHERS DU NET.</p>
+        <p><a href="/">Revenir sur la page d'acceuil</a></p>
 
         <footer>
             <p>&copy; Copyright  {{date('Y')}} &middot; <a href="/about-us">About Us</a></p>
