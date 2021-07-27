5f51ee2229aa324897cbdb3570daa7b84a557210
Bayero Abass Barry <bayeroabassb@gmail.com>
2021-07-27 19:07:59 +0000

    remove duplication code

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,22 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>About us | Hello city</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <p>Built with &hearts; by LES TEACHERS DU NET.</p>
 
+    <p><a href="/">Revenir sur la page d'acceuil</a></p>
 
-    <body>
-
-        <p>Built with &hearts; by LES TEACHERS DU NET.</p>
-        <p><a href="/">Revenir sur la page d'acceuil</a></p>
-
-        <footer>
-            <p>&copy; Copyright  {{date('Y')}}</p>
-        </footer>
-    </body>
-</html>
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,23 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>Hello city</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-
-    <body>
+@section('content')
         <h1>Hello from Conakry !</h1>
 
         <p>It's currently {{date('h:i A')}}.</p>
 
-
-        <footer>
-            <p>&copy; Copyright  {{date('Y')}} &middot; <a href="/about-us">About Us</a></p>
-        </footer>
-    </body>
-</html>
+@endsection
