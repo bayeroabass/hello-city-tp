c1c677b74b6258f804bf9ae9fdec6adc3308079e
Bayero Abass Barry <bayeroabassb@gmail.com>
2021-07-28 14:17:05 +0000

    Do not dislay about in about page

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::name('home')->get('/', function () {
     return view('pages.home');
-})->name('home');
+});
 Route::get('/about-us',function(){
     return view('pages/about');
 })->name('about');
