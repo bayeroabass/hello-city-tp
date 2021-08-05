2ced48c25c7706342b2e13fd1a7a5702065bd799
Bayero Abass Barry <bayeroabassb@gmail.com>
2021-08-05 06:57:06 +0000

    Correction des erreurs sur l'appli

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 Route::name('home')->get('/', function () {
     return view('pages.home');
 });
-Route::get('/about-us',function(){
+Route::name('about')->get('/about-us',function(){
     return view('pages/about');
-})->name('about');
+});
